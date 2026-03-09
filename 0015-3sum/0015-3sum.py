e6c8b5fee58eba6cbc771a788114b5788b94750c
Izza Fatima <izzahfatima044@gmail.com>
2026-03-09 16:53:31 +0500

    Time: 671 ms (31.94%), Space: 22.2 MB (92.09%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,35 +1,33 @@
-class Solution(object):
-    def threeSum(self, nums):
-        arr=[]
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
         nums.sort()
+        ans = []
 
-        for i in range(len(nums)-2):
-            if i>0 and nums[i]==nums[i-1]:
+        for i in range(len(nums) - 2):
+
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
 
-            l=i+1
-            r=len(nums)-1
+            l = i + 1
+            r = len(nums) - 1
 
-            while l<r:
-                total=nums[i]+nums[l]+nums[r]
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
 
-                if total>0:
-                    r-=1
+                if s == 0:
+                    ans.append([nums[i], nums[l], nums[r]])
+                    l += 1
+                    r -= 1
 
-                elif total<0:
-                    l+=1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
 
+                    while l < r and nums[r] == nums[r+1]:
+                        r -= 1
+
+                elif s < 0:
+                    l += 1
                 else:
-                    arr.append([nums[i],nums[l],nums[r]])
+                    r -= 1
 
-                    while l<r and nums[l]==nums[l+1]:
-                        l+=1
-
-                    while l<r and nums[r]==nums[r-1]:
-                        r-=1
-
-                    l+=1
-                    r-=1
-
-
-        return arr
+        return ans
